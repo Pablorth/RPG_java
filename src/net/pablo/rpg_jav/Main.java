7abf7ffabf16712bc852933a8a630142b21e4ab5
Pablorth <pablo.routhier@gmail.com>
2021-11-25 14:22:34 +0100

    amelioration v2 main.java

--- src/net/pablo/rpg_jav/Main.java
+++ src/net/pablo/rpg_jav/Main.java
@@ -6,48 +6,98 @@ public class Main {
 
 	public static void main(String[] args) {
 
-
-		
 		int start = 0;
-		if(start == 0) {
+		while (start == 0) {
 			System.out.println("Joueur 1: choisi ton Champion!");
 			System.out.println("Pour le Chavalier : tape 1");
 			System.out.println("Pour le Mage : tape 2");
 			System.out.println("Pour l'Archer : tape 3");
 			Scanner scan = new Scanner (System.in);
 			int choice = scan.nextInt();
-			if (choice != 1 && choice != 2 && choice !=3) {
+			while (choice != 1 && choice != 2 && choice !=3) {
 					System.out.println("Tu a le choix entre 1 , 2 ou 3!!!");
 				System.out.println("Joueur 1: choisi ton Champion!");
 				System.out.println("Pour le Chavalier : tape 1");
 				System.out.println("Pour le Mage : tape 2");
 				System.out.println("Pour l'Archer : tape 3");
 				scan = new Scanner (System.in);
+				choice = scan.nextInt();
 				
-			} else {
-				if(choice == 1) {
-					System.out.println("Le Chavalier! Choisie lui un nom!!");
-					Scanner scanName = new Scanner (System.in);
-					Knight player1 = new Knight(scanName.nextLine(), 300, 50);
-					
-				} 
-				else if(choice == 2) {
-					System.out.println("Le Mage! Choisie lui un nom!!");
-					Scanner scanName = new Scanner (System.in);
-					Mage player1 = new Mage(scanName.nextLine(), 300, 50);
-					
-				}
-				else if(choice == 3) {
-					System.out.println("Le l'Archer! Choisie lui un nom!!");
-					Scanner scanName = new Scanner (System.in);
-					Archer player1 = new Archer(scanName.nextLine(), 300, 50);
-					
-				}
+			} 
+			
+			if(choice == 1) {
+				System.out.println("Le Chavalier! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Knight player1 = new Knight(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player1.getName());
+				start = 0;
+				
+			} 
+			else if(choice == 2) {
+				System.out.println("Le Mage! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Mage player1 = new Mage(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player1.getName());
+				start = 1;
+				
+			}
+			else if(choice == 3) {
+				System.out.println("Le l'Archer! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Archer player1 = new Archer(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player1.getName());
+				start = 1;
+				
 			}
 			
-		
 			System.out.println(scan.nextInt());
 			System.out.println(scan.nextLine());
+			
+		} 
+		
+		if (start == 1) {
+			
+			System.out.println("Joueur 2: choisi ton Champion!");
+			System.out.println("Pour le Chavalier : tape 1");
+			System.out.println("Pour le Mage : tape 2");
+			System.out.println("Pour l'Archer : tape 3");
+			Scanner scan = new Scanner (System.in);
+			int choice = scan.nextInt();
+			while (choice != 1 && choice != 2 && choice !=3) {
+				System.out.println("Tu a le choix entre 1 , 2 ou 3!!!");
+				System.out.println("Joueur 2: choisi ton Champion!");
+				System.out.println("Pour le Chavalier : tape 1");
+				System.out.println("Pour le Mage : tape 2");
+				System.out.println("Pour l'Archer : tape 3");
+				scan = new Scanner (System.in);
+				choice = scan.nextInt();
+				
+			} 
+			
+			if(choice == 1) {
+				System.out.println("Le Chavalier! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Knight player2 = new Knight(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player2.getName());
+				
+			} 
+			else if(choice == 2) {
+				System.out.println("Le Mage! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Mage player2 = new Mage(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player2.getName());
+				
+			}
+			else if(choice == 3) {
+				System.out.println("Le l'Archer! Choisie lui un nom!!");
+				Scanner scanName = new Scanner (System.in);
+				Archer player2 = new Archer(scanName.nextLine(), 300, 50);
+				System.out.println("Bienvenu " + player2.getName());
+				
+			}
+				start = 1;
+			
+			
 		}
 		
 
