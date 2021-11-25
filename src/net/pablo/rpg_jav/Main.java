87ac3696895a4902d6f31228a95c37f72c5e5230
Pablorth <pablo.routhier@gmail.com>
2021-11-25 13:52:48 +0100

    modification main

--- src/net/pablo/rpg_jav/Main.java
+++ src/net/pablo/rpg_jav/Main.java
@@ -6,35 +6,30 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Player player5 = new Player("Roger", 300, 50);
-		Player player6= new Player("Jean", 300, 50);
-		
-		System.out.println("name :" + player5.getName());
-		System.out.println("life :" + player5.getHealth());
-		player5.damage(player6.getAtk());
-		System.out.println("life :" + player5.getHealth());
+
 		
 		int start = 0;
 		if(start == 0) {
-			System.out.println("Joueur 1 choisi Choisi son Champion!");
+			System.out.println("Joueur 1: choisi ton Champion!");
 			System.out.println("Pour le Chavalier : tape 1");
 			System.out.println("Pour le Mage : tape 2");
 			System.out.println("Pour l'Archer : tape 3");
 			Scanner scan = new Scanner (System.in);
-			if (scan.nextInt() != 1 && scan.nextInt() != 2 && scan.nextInt() !=3) {
-				if(scan.nextInt() == 1) {
+			int choice = scan.nextInt();
+			if (choice != 1 && choice != 2 && choice !=3) {
+				if(choice == 1) {
 					System.out.println("Le Chavalier! Choisie lui un nom!!");
 					Scanner scanName = new Scanner (System.in);
 					Player player1 = new Player(scanName.nextLine(), 300, 50);
 					
 				} 
-				else if(scan.nextInt() == 2) {
+				else if(choice == 2) {
 					System.out.println("Le Mage! Choisie lui un nom!!");
 					Scanner scanName = new Scanner (System.in);
 					Player player1 = new Player(scanName.nextLine(), 300, 50);
 					
 				}
-				else if(scan.nextInt() == 3) {
+				else if(choice == 3) {
 					System.out.println("Le l'Archer! Choisie lui un nom!!");
 					Scanner scanName = new Scanner (System.in);
 					Player player1 = new Player(scanName.nextLine(), 300, 50);
@@ -42,13 +37,12 @@ public class Main {
 				}	
 			} else {
 				System.out.println("Tu a le choix entre 1 , 2 ou 3!!!");
-				System.out.println("Joueur 1 choisi Choisi son Champion!");
+				System.out.println("Joueur 1: choisi ton Champion!");
 				System.out.println("Pour le Chavalier : tape 1");
 				System.out.println("Pour le Mage : tape 2");
 				System.out.println("Pour l'Archer : tape 3");
 				scan = new Scanner (System.in);
 			}
-			
 			
 		
 			System.out.println(scan.nextInt());
